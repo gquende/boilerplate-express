de7a5befac36743d4cafd936cee10774159a4a29
Geraldo Quende <43283373+gquende@users.noreply.github.com>
2023-09-11 16:28:35 +0100

    FEAT: Use middlware

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ let express = require("express");
 let app = express();
 
 const homepage = __dirname + "/views/index.html";
+const publicPath = __dirname + "/public";
+
+app.use("/public", express.static(publicPath));
 
 app.get("/", (req, res) => {
   res.sendFile(homepage);
