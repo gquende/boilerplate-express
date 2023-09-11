182c792dc872306daf19e18f7f23b4bf829bf9d2
Geraldo Quende <43283373+gquende@users.noreply.github.com>
2023-09-11 15:33:12 +0100

    FEAT: Send Html file to client

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+const homepage = __dirname + "/views/index.html";
 
+app.get("/", (req, res) => {
+  res.sendFile(homepage);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
